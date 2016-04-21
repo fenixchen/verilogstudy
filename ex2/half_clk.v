5e492f41c3f4bb5375482085b6b7258a57c92618
chenchanghong <changhongchen@rdamicro.com>
2016-04-21 17:31:28 +0800

    Add counter register

--- ex2/half_clk.v
+++ ex2/half_clk.v
@@ -2,9 +2,10 @@ module half_clk(reset, clk_in, clk_out);
     input clk_in, reset;
     output clk_out;
     reg clk_out;
+    reg[7:0] clk_cnt;
     always @(negedge clk_in)
     begin
-        if (!reset) clk_out = 0;
-        else clk_out = ~clk_out;
+	    if (!reset) begin clk_out = 0; clk_cnt = 0; end
+	    else begin clk_out = ~clk_out; clk_cnt = clk_cnt + 1; end
     end
 endmodule
