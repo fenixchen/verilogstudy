421ac4c3040e42ad6d84aa97d9ebe7d719cedd5a
chenchanghong <changhongchen@rdamicro.com>
2016-07-12 09:51:33 +0800

    more

--- ex2/half_clk.v
+++ ex2/half_clk.v
@@ -3,9 +3,23 @@ module half_clk(reset, clk_in, clk_out);
     output clk_out;
     reg clk_out;
     reg[7:0] clk_cnt;
+    reg clk_copy;
     always @(negedge clk_in)
     begin
-	    if (!reset) begin clk_out = 0; clk_cnt = 0; end
-	    else begin clk_out = ~clk_out; clk_cnt = clk_cnt + 1; end
+	    if (!reset) begin clk_out <= 0; clk_cnt <= 0; end
+	    else begin clk_out <= ~clk_out; clk_cnt <= clk_cnt + 1; end
     end
+
+    always @(clk_in) clk_copy <= clk_in;
+
+    reg[7:0] ccc;
+    reg[7:0] ddd;
+    always @(posedge clk_in)
+    begin
+	    if (!reset) begin ccc <= 0; ddd <= 0; end
+	    else begin 
+		    if (clk_in) ccc <= ccc + 1;
+		    if (clk_copy) ddd <= ddd + 1; 
+	    end
+    end 
 endmodule
